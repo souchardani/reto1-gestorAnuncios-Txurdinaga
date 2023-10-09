1f35a86bf7e43ca7a25c6f2bd6a0ace2c2504b9a
carmengd <102661419+carmengd2003@users.noreply.github.com>
2023-10-09 14:10:11 +0200

    todo organizado

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CIFP TXURDINADA LHII - Formamos profesionales de hoy y de mañana</title>
+    <link rel="shortcut icon" href="assets/img/FP_ico.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php
+        include_once "header.html";
+    ?>
+    <?php
+        include_once "footer.html";
+    ?>
+</body>
+</html>
