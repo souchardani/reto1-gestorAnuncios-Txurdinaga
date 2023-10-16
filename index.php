9429724cdf1f39d8e58a60ab3e79e16cfe734982
Ander200 <andertapiafer@gmail.com>
2023-10-16 09:21:06 +0200

    Cambios Pequeños

--- index.php
+++ index.php
@@ -1,163 +1,226 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CIFP TXURDINADA LHII - Formamos profesionales de hoy y de mañana</title>
-    <link rel="shortcut icon" href="assets/img/FP_ico.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/style.css" />
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <title>Inicio</title>
+  <!-- LIBRERÍA DE ICONOS -->
+  <script src="https://unpkg.com/@phosphor-icons/web"></script>
+</head>
+
+<body>
+  <header>
+    <div>
+      <a href="Inicio.html">
+        <img src="../assets/img/FP-header.png" alt="logo" id="logo">
+      </a>
+    </div>
+  
+    <div>
+      <a href="Editar-usuarios.html">
+        <img src="../assets/img/doffy.jpeg" alt="perfil" id="foto-perfil">
+      </a>
+    </div>
+  </header>
     
-    <!-- LIBRERIA DE ICONOS -->
-    <script src="https://unpkg.com/@phosphor-icons/web"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  </head>
-  <body>
-    <?php
-      include_once "header.html";
-    ?>
-    <section id="home-hero">
-      <div class="container flex">
-        <div class="left">
-          <h3 class="display-03 flex">
-            Descubre todas las Noticias y Anuncios de cifp Txurdinaga
-          </h3>
-          <p class="body-medium-400">Enterate de todas nuestras novedades!</p>
-        </div>
-        <img src="assets/img/logocuadro.png" alt="" />
+  <nav id="nav-links">
+      <ul>
+        <a id="li-inicio" href="Inicio.html"><li >Inicio</li></a>
+        <a id="li-perfil" href="Editar-usuarios.html"><li >Perfil</li></a>
+        <a id="li-crear" href="Crear-anuncio.html"><li >Crear anuncio</li></a>
+        <li id="li-buscador">
+          <form id="buscador" action="" method="POST">
+              <div>
+                  <input id="busqueda" type="search" name="busqueda" placeholder="Bilatu liburua..." value="">
+              </div>
+              <div>
+                  <button id="buscar" type="submit"><img class="search-img" src="../assets/img/search.png" alt=""></button>
+              </div>
+          </form>
+        </li>
+      </ul>
+  </nav>
+  
+  <section id="home-hero">
+    <div class="container flex">
+      <div>
+        <h3 class="display-03">Descubre todas las Noticias y Anuncios de CIFP Txurdinaga</h3>
+        <p class="body-medium-400">Entérate de todas nuestras novedades!</p>
       </div>
-    </section>
+      <div>
+        <img src="../assets/img/FP-inicio.png" alt="logo-inicio"/>
+      </div>
+    </div>
+  </section>
 
-    <section id="pagina-inicio">
-      <div class="container">
-        <h2 class="display-03">Anuncios Recientes</h2>
-        <div class="columnas-anuncios flex">
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
+  <section id="pagina-inicio">
+    <div class="container">
+      <h2 class="display-03">Anuncios Recientes</h2>
+      <div class="columnas-anuncios flex">
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
             </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
           </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
             </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
-          </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
-            </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
-          </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
-            </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
-          </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
-            </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
-          </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
-            </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
-          </div>
-          <div class="anuncio">
-            <img src="assets/img/dates-icon.jpg" alt="" />
-            <div class="anuncio-body flex">
-              <div class="categoria-info flex">
-                <i class="ph ph-stack"></i>
-                <span class="body-small-400">Texto Categoria</span>
-              </div>
-              <h3 class="body-medium-600">Titulo del anuncio</h3>
-            </div>
-            <div class="anuncio-info flex">
-              <div class="anuncio-lugar flex">
-                <i class="ph ph-map-pin"></i>
-                <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
-              </div>
-              <span class="fecha">06/10/2023</span>
-            </div>
+            <span class="fecha">06/10/2023</span>
           </div>
         </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+
+        <div class="anuncio">
+          <img class="img-anun" src="../assets/img/dates-icon.jpg" alt="" />
+          <div class="anuncio-body flex">
+            <div class="categoria-info flex">
+              <i class="ph ph-stack"></i>
+              <span class="body-small-400">Texto Categoría</span>
+            </div>
+            <h3 class="body-medium-600">Titulo del anuncio</h3>
+          </div>
+          <div class="anuncio-info flex">
+            <div class="anuncio-lugar flex">
+              <i class="ph ph-map-pin"></i>
+              <span class="body-small-400 anuncio-ubicacion"> Bilbao</span>
+            </div>
+            <span class="fecha">06/10/2023</span>
+          </div>
+        </div>
+        
       </div>
-    </section>
-    <?php
-      include_once "footer.html";
-    ?>
-  </body>
+    </div>
+  </section>
+
+  <footer>
+    <div class="footer-col1">
+        <a href="Inicio.html"><img id="footer-logo" src="../assets/img/FP-footer.png"></a>
+        <div id="datos1">C/ Doctor Ornilla 2</div>
+        <div id="datos2">48004 Bilbao</div>
+        <div id="datos3">Telefono: +34 94 412 57 12</div>
+        <div id="datos4">Email: idazkaria@fpTXurdinaga.com</div>
+    </div>
+    <div class="footer-col2">
+        <div>
+            <img class="socials-img" src="../assets/img/insta.png" alt="insta">
+            <img class="socials-img" src="../assets/img/twitter.png" alt="twitter">
+            <img class="socials-img" src="../assets/img/linkedin.png" alt="LinkedIn">
+        </div>
+        <p id="datos5">POLÍTICA DE PRIVACIDAD</p>
+        <p id="datos6">COOKIES</p>
+        <p id="datos7">MAPA WEB</p>
+        <p id="datos8">CONTACTO</p>
+    </div>
+    <div class="footer-col3">
+        <div class="logos-arriba">
+            <img src="../assets/img/48005.png" class="footer-emp-img">
+            <img src="../assets/img/48007.png" class="footer-emp-img">
+            <img src="../assets/img/Lanbide 1.png" class="footer-emp-img">
+        </div>
+        <div class="logos-abajo">
+            <img src="../assets/img/48006.png" class="footer-emp-img">
+            <img src="../assets/img/image 1.png" class="footer-emp-img">
+        </div>
+    </div>
+  </footer>
+  
+</body>
 </html>
