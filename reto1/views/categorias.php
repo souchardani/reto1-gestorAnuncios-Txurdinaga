b82d72324da3d48a96cfec758985ff1f1b6166a2
souchardani <114416167+souchardani@users.noreply.github.com>
2023-10-22 22:44:00 +0200

    terminadas tablas

--- reto1/views/categorias.php
+++ reto1/views/categorias.php
@@ -35,9 +35,7 @@ if(isset($_POST["enviar"])){
     <!-- HEADER END -->
 
   <!-- MAIN AREA -->
-  <section class="container py-2 mb-4">
-    <div class="row" >
-      <div class="offset-lg-1 col-lg-10" style="min-height: 50vh;">
+  <section class="container">
       <?php 
       echo MensajeError();
       echo MensajeExito();
@@ -52,47 +50,55 @@ if(isset($_POST["enviar"])){
               <label class="mb-3" for="title"><span class="FieldInfo">Titulo de la Categoria:</span></label>
               <input class="form-control " type="text" name="tituloCategoria" id="title" placeholder="Escribe el titulo aqui">
             </div>
-            <div class="row">
-            <div class="col-lg-6 mb-2">
-              <a class="btn btn-warning d-lg-block w-100" href="dashboard.php"><i class="fa-solid fa-arrow-left"></i> Volver al Panel de Control</a>
+            <div class="d-flex">
+            <div>
+              <a class="boton tx-amarillo w-100" href="dashboard.php"><i class="fa-solid fa-arrow-left"></i> Volver al Panel de Control</a>
             </div>
-            <div class="col-lg-6 mb-2  d-md-block ">
-              <button type="submit" name="enviar" class="btn btn-success w-100"><i class="fa-solid fa-check"></i> Publicar</button>
+            <div>
+              <button type="submit" name="enviar" class="boton tx-verde-claro w-100"><i class="fa-solid fa-check"></i> Publicar</button>
             </div>
           </div>
           </div>
         </div>
-
       </form>
-      <h2>Categorias</h2>
-          <table class="table table-stripped table-hover">
-            <thead class="table-dark">
+
+         <!-- tabla categorias -->
+         <main class="table mt-bg">
+        <section class="table__header">
+          <h1 class="heading-02">Categorias</h1>
+          <div class="input-group">
+              <input type="search" name="" id="" placeholder="Buscar" />
+              <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </section>
+        <section class="table__body">
+          <table class="table-center">
+          <thead>
               <tr>
                 <th>Nº</th> 
                 <th>Nombre</th>
                 <th>Eliminar</th>
               </tr>
-            </thead>
+          </thead>
+        <tbody>         
           <?php
-          $stmt = obtener_categorias();
-          $contador = 0;
-          while ($fila = $stmt -> fetch()){          
-            $titulo = $fila["Nombre"];
-            $contador++;
-          ?>
-          <tbody>
-            <tr>
-              <td><?php echo $contador; ?></td>
-              <td><?php echo $titulo; ?></td>
-              <td><a href="eliminar_categoria.php?id=<?php echo $titulo; ?>" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></a></td>
-            </tr>
-          </tbody>
-          <?php } ?>
-          </table>  
-    </div>
-    </div>
-  </section>
-
+            $stmt = obtener_categorias();
+            $contador = 0;
+            while ($fila = $stmt -> fetch()){          
+              $titulo = $fila["Nombre"];
+              $contador++;
+            ?>
+              <tr>
+                <td><?php echo $contador; ?></td>
+                <td><?php echo $titulo; ?></td>
+                <td><a onclick="return confirm('Debes verificar que no hay anuncios creados con esta categoria, de lo contrario, no se podra eliminar')" href="eliminar_categoria.php?id=<?php echo $titulo; ?>" class="boton rojo"><i class="fa-solid fa-trash-can"></i></a></td>
+              </tr>
+            <?php } ?>
+              </tbody>
+            </table>
+          </section>
+        </main>
+    </section>
   <!-- END MAIN AREA -->
     <!-- FOOTER -->
     <?php include("../templates/footer.php"); ?>
