6086d262d6da988055374af0d41e02d3c2aefd89
souchardani <114416167+souchardani@users.noreply.github.com>
2023-10-24 13:18:59 +0200

    pequeños cambios

--- reto1/views/anuncio_completo.php
+++ reto1/views/anuncio_completo.php
@@ -139,27 +139,6 @@ if(isset($_POST["Enviar"])){
 
         </div>
         <!-- fin seccion principal -->
-        <!-- inicio aside area -->
-        <div class="col-sm-4" style="min-height: 40px; ">
-          <!-- tarjeta categorias -->
-          <div class="card my-5">
-            <div class="card-header text-bg-light text-center">
-              <h2 class="lead">Categorias</h2>
-              <div class="card-body">
-                <ul class="list-group">
-                <?php
-                $categorias = obtener_categorias();
-                while ($fila = $categorias -> fetch()){
-                  $categoria = $fila["Nombre"];
-                  echo "<a href='anuncios_inicio.php?categoria=$categoria' class='list-group-item list-group-item-action'>$categoria</a>";
-                }
-                ?>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- fin aside area -->
       </div>
     </div>
     <!-- HEADER END -->
