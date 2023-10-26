6a900bc4dad23116bc375f7784bb37e82f9d6583
souchardani <114416167+souchardani@users.noreply.github.com>
2023-10-26 14:30:00 +0200

    añadir db local

--- reto1/assets/includes/DB.php
+++ reto1/assets/includes/DB.php
@@ -4,6 +4,11 @@ try {
     $Conexionbbdd = new PDO($myPDO2, "admin", "admin123");
     $Conexionbbdd->exec("set names utf8");
 
+    //conexion local para cuando aws este apagado
+    // $myPDO2 = "mysql:host=localhost;dbname=gestor_anuncios";
+    // $Conexionbbdd = new PDO($myPDO2, "root", "");
+    // $Conexionbbdd->exec("set names utf8");
+
 }catch(PDOException $e){
     echo "Error al conectar a la base de datos. Error: $e";
 }
