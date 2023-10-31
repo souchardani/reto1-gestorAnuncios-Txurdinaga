589ed6ed4d4560885dfe54d5b8016c03f1212efd
souchardani <114416167+souchardani@users.noreply.github.com>
2023-10-31 08:59:23 +0100

    hashear contraseña

--- reto1/assets/includes/funciones.php
+++ reto1/assets/includes/funciones.php
@@ -26,6 +26,8 @@ function comprobar_variable_url($variable_url, $ubicacion){
 
 function inicio_sesion($usuario, $password){
   global $Conexionbbdd;
+  //cifrarcontraseña
+  $password = hash('sha1',$password);
   $sql = "SELECT * FROM usuario WHERE BINARY Nick=:usuario AND BINARY Contraseña=:password and Activo=1 LIMIT 1";
   $Stmt = $Conexionbbdd -> prepare($sql);
   $Stmt -> bindValue(":usuario", $usuario);
@@ -512,7 +514,7 @@ function validar_data_user($username, $contrasena, $confirmar_contrasena, $ubica
 
 
 //funcion para verificar la existencia del administrador
-function verificar_existencia_user($username) {
+function verificar_existencia_user($username, $ubicacion) {
   global $Conexionbbdd;
   $sql = "SELECT * FROM usuario WHERE Nick=:Nick";
   $stmt = $Conexionbbdd -> prepare($sql);
@@ -521,7 +523,7 @@ function verificar_existencia_user($username) {
   $resultado = $stmt -> rowCount();
   if($resultado == 1){
     $_SESSION["MensajeError"] = "El nombre de usuario ya existe, prueba con otro";
-    Redireccionar_A("users.php");
+    Redireccionar_A($ubicacion);
     return false;
   }else {
     return true;
