dd783a4e02aa8954a1220ab3eea6f6b04948f837
souchardani <114416167+souchardani@users.noreply.github.com>
2023-11-02 22:37:59 +0100

    correccion busqueda en tablas

--- reto1/assets/js/tablas.js
+++ reto1/assets/js/tablas.js
@@ -1,10 +1,12 @@
-const buscar = document.querySelector(".input-group input"),
+const buscar = document.querySelectorAll(".input-group input"),
   filas_tabla = document.querySelectorAll("tbody tr");
 
-buscar.addEventListener("input", buscarEnTabla);
+buscar.forEach((input) => {
+  input.addEventListener("input", buscarEnTabla);
+});
 
-function buscarEnTabla() {
-  let texto_busqueda = buscar.value.toLowerCase();
+function buscarEnTabla(event) {
+  let texto_busqueda = event.target.value.toLowerCase();
 
   filas_tabla.forEach((fila, i) => {
     let texto_tabla = fila.textContent.toLowerCase();
