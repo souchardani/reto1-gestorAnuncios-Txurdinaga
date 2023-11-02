c1cf12b4ba0f29f28b60ed0b5f343948dff63166
souchardani <114416167+souchardani@users.noreply.github.com>
2023-11-02 12:14:48 +0100

    validacion de registro limpiar colores

--- reto1/assets/js/registro.js
+++ reto1/assets/js/registro.js
@@ -6,7 +6,8 @@ const pass2 = document.getElementById("pass2");
 const fecha = document.getElementById("fecha");
 
 // Regex 6 caracteres, una mayuscula y una minuscula como minimo
-const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d{6,}).*$/;
+// const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d{5,}).*$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z]).{6,}$/;
 
 // Funcion que calcula la edad (minimo 15 años)
 fecha.addEventListener("change", function () {
@@ -16,10 +17,7 @@ fecha.addEventListener("change", function () {
   const edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
 
   if (edad < 15) {
-    fecha.classList.add("rojo");
     alert("Debes tener al menos 15 años para registrarte");
-  } else {
-    fecha.classList.add("verde");
   }
 });
 
@@ -28,59 +26,65 @@ function validarContrasena(contrasena) {
   return passwordRegex.test(contrasena);
 }
 
-// Validacion de los inputs
-inputs.forEach((input) => {
-  input.addEventListener("keyup", function () {
-    if (input.value === "") {
-      input.classList.remove("azul", "verde");
-      input.classList.add("rojo");
-    } else {
-      input.classList.remove("rojo");
-      if (input.validity.valid) {
-        input.classList.add("verde");
-      } else {
-        input.classList.add("azul");
-      }
-    }
-  });
-});
+// // Validacion de los inputs por colores
+// inputs.forEach((input) => {
+//   input.addEventListener("keyup", function () {
+//     if (input.value === "") {
+//       input.classList.remove("azul", "verde");
+//       input.classList.add("rojo");
+//     } else {
+//       input.classList.remove("rojo");
+//       if (input.validity.valid) {
+//         input.classList.add("verde");
+//       } else {
+//         input.classList.add("azul");
+//       }
+//     }
+//   });
+// });
 
-// Validacion de los select
-selects.forEach((select) => {
-  select.addEventListener("click", function () {
-    if (select.value === "") {
-      select.classList.remove("azul", "verde");
-      select.classList.add("rojo");
-    } else {
-      select.classList.remove("rojo");
-      if (select.validity.valid) {
-        select.classList.add("verde");
-      } else {
-        select.classList.add("azul");
-      }
-    }
-  });
-});
+// Validacion de los select por colores
+// selects.forEach((select) => {
+//   select.addEventListener("click", function () {
+//     if (select.value === "") {
+//       select.classList.remove("azul", "verde");
+//       select.classList.add("rojo");
+//     } else {
+//       select.classList.remove("rojo");
+//       if (select.validity.valid) {
+//         select.classList.add("verde");
+//       } else {
+//         select.classList.add("azul");
+//       }
+//     }
+//   });
+// });
 
 // Comprueba que las contraseñas coinciden
-pass2.addEventListener("keyup", function () {
-  if (pass1.value === pass2.value) {
-    pass1.classList.remove("rojo");
-    pass1.classList.add("verde");
-    pass2.classList.remove("rojo");
-    pass2.classList.add("verde");
-  } else {
-    pass1.classList.remove("verde");
-    pass1.classList.add("rojo");
-    pass2.classList.remove("verde");
-    pass2.classList.add("rojo");
-  }
+pass1.addEventListener("blur", function () {
+  // if (pass1.value === pass2.value) {
+  //   pass1.classList.remove("rojo");
+  //   pass1.classList.add("verde");
+  //   pass2.classList.remove("rojo");
+  //   pass2.classList.add("verde");
+  // } else {
+  //   pass1.classList.remove("verde");
+  //   pass1.classList.add("rojo");
+  //   pass2.classList.remove("verde");
+  //   pass2.classList.add("rojo");
+  // }
 
-  if (validarContrasena(pass1.value)) {
-    pass1.classList.remove("azul");
-    pass1.classList.add("verde");
-  } else {
-    pass1.classList.remove("azul");
-    pass1.classList.add("verde");
+  if (!validarContrasena(pass1.value)) {
+    alert(
+      "La contraseña debe tener al menos 6 caracteres, una mayuscula y una minuscula como minimo"
+    );
+  }
+});
+
+pass2.addEventListener("blur", function () {
+  if (!(pass1.value === pass2.value)) {
+    alert(
+      "Las contraseñas no coinciden, por favor, introduzca la misma contraseña en ambos campos"
+    );
   }
 });
